5d3d46e00962f7f856d8e8bc6cebcfe6047c20c2
daniel <contact@dnlzrgz.com>
2026-05-10 16:35:00 +0200

    test(schemas): update tests for validation in schemas

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -1,7 +1,7 @@
 import pytest
 
-from jobless.enums import Location, Status
 from jobless.schemas import Application, Company, Contact, Skill
+from tests import factories
 
 
 def test_skill_name_validation():
@@ -15,13 +15,11 @@ def test_company_name_validation():
 
 
 def test_application_title_validation():
-    from tests.factories import make_company_schema
-
     with pytest.raises(ValueError):
         Application(
             id=1,
             title="\n\t",
-            company=make_company_schema(),
+            company=factories.CompanyFactory.build(),
         )
 
 
@@ -31,15 +29,3 @@ def test_contact_email_validation():
 
     with pytest.raises(ValueError):
         Contact(id=1, name="John Doe", email="not valid")
-
-
-def test_minimal_application_creation():
-    from tests.factories import make_company_schema
-
-    application = Application(
-        id=1,
-        title="Software Engineer",
-        company=make_company_schema(),
-    )
-    assert application.status == Status.SAVED
-    assert application.location_type == Location.ON_SITE
